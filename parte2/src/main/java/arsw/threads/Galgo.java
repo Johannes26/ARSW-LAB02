af2022303cf09e41d0e6cbb9e010be95d0efe49e
Jonathan Paez <jfpazto@hotmail.com>
2021-01-31 12:23:22 -0500

    parte terminado

--- parte2/src/main/java/arsw/threads/Galgo.java
+++ parte2/src/main/java/arsw/threads/Galgo.java
@@ -7,37 +7,56 @@ package arsw.threads;
  * 
  */
 public class Galgo extends Thread {
+	boolean pausar;
 	private int paso;
 	private Carril carril;
 	RegistroLlegada regl;
 
 	public Galgo(Carril carril, String name, RegistroLlegada reg) {
+		
 		super(name);
 		this.carril = carril;
 		paso = 0;
 		this.regl=reg;
+		this.pausar=false;
 	}
 
-	public void corra() throws InterruptedException {
-		while (paso < carril.size()) {			
+	public void corra()  throws InterruptedException {
+		while (paso < carril.size()) {	
+			synchronized (this){
+				if (pausar){
+					wait();
+				}
+			}
 			Thread.sleep(100);
 			carril.setPasoOn(paso++);
 			carril.displayPasos(paso);
 			
 			if (paso == carril.size()) {						
 				carril.finish();
-				int ubicacion=regl.getUltimaPosicionAlcanzada();
-				regl.setUltimaPosicionAlcanzada(ubicacion+1);
-				System.out.println("El galgo "+this.getName()+" llego en la posicion "+ubicacion);
-				if (ubicacion==1){
-					regl.setGanador(this.getName());
+				synchronized (regl)
+				{
+					int ubicacion=regl.getUltimaPosicionAlcanzada();
+					regl.setUltimaPosicionAlcanzada(ubicacion+1);
+					System.out.println("El galgo "+this.getName()+" llego en la posicion "+ubicacion);
+					if (ubicacion==1){
+						regl.setGanador(this.getName());
+					}
 				}
 				
 			}
 		}
 	}
 
-
+    synchronized void pausarhilo(){
+        pausar=true;
+        notifyAll();
+    }
+    
+    synchronized void reanudarhilo(){
+        pausar=false;
+        notifyAll();
+    }
 	@Override
 	public void run() {
 		
